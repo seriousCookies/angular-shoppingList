9ea5f81870e09f1b7076cd2cb3b4c78b0eaf233c
Na Wei <na.wei95@hotmail.com>
2021-02-11 15:13:31 +0100

    working todo

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Todo} from './../../models/Todo';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+todos:Todo[];
+inputTodo:string="";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.todos=[
+    ]
   }
 
+  toggleDone(id:number) {
+    this.todos.map((el, i)=> {
+      if (i==id) el.completed = !el.completed;
+      return el;
+    })
+  }
+  deleteToDo(id:number){
+    this.todos=this.todos.filter((v,i)=> i!==id);
+  }
+  addToDo() {
+    this.todos.push({
+      content:this.inputTodo,
+      completed: false
+    })
+    this.inputTodo="";
+  }
 }
