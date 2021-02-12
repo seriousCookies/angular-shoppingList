3e164f6510cd3a89f86ba6d7af2cad1327a09f6e
Na Wei <na.wei95@hotmail.com>
2021-02-12 10:22:50 +0100

    more styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -18,6 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     MatCardModule,
+    MatCheckboxModule,
     FormsModule,
     MatInputModule,
     MatButtonModule,
